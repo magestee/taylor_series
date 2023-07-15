fa8aebd535148572a630bc6d10cb552e008c08d6
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-07-15 21:58:29 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+""" math module: """
+# import math
+
+# x = 2
+# n = 20
+# taylor_term = 0
+
+# for i in range(n):
+#     taylor_term += x**i/math.factorial(i)
+
+# print(taylor_term)
+# print(math.exp(2))
+
+""" numpy """
